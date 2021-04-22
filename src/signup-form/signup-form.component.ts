38882194fbdb65452ffca6eaee973b758bbf6109
tonyg <aj.gianno@gmail.com>
2021-04-22 18:55:06 -0500

    formGroup and formControl imports in signup-form component

--- src/signup-form/signup-form.component.ts
+++ src/signup-form/signup-form.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+  styleUrls: ['./signup-form.component.css'],
 })
-export class SignupFormComponent {
-}
+export class SignupFormComponent {}
