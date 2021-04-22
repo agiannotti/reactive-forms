63cd342a10adc3b64c8516dda3c4db1ee768c746
tonyg <aj.gianno@gmail.com>
2021-04-22 18:43:35 -0500

    signup-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { SignupFormComponent } from '../signup-form/signup-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SignupFormComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
