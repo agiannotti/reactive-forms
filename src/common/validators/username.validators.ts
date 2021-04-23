bb196db5a630724430fef27e1843aa2b55b68fc3
tonyg <aj.gianno@gmail.com>
2021-04-22 20:41:27 -0500

    double check

---
+++ src/common/validators/username.validators.ts
@@ -0,0 +1,18 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+// UsernameValidators.cannotContainSpace
+
+// to make this validator method available, define method as static
+export class UsernameValidators {
+  //custom validator methods to construct interface passed to formGroup
+  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+    if ((control.value as string).indexOf(' ') >= 0)
+      return { cannotContainSpace: true };
+
+    return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+    if (control.value === 'tony') return { shouldBeUnique: true };
+    return null;
+  }
+}
