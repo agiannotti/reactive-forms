b106367fbd5cca3f98bd3d83abcfc60821778438
tonyg <aj.gianno@gmail.com>
2021-04-22 19:04:52 -0500

    FormGroup object creation

--- src/signup-form/signup-form.component.ts
+++ src/signup-form/signup-form.component.ts
@@ -6,4 +6,15 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css'],
 })
-export class SignupFormComponent {}
+export class SignupFormComponent {
+  // abstract control acts as base control for formControl and formGroup
+
+  // all properties that are common between these two classes
+  // will be stored in the abstract control!
+
+  // this is how to explicitly create form objects
+  form = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+}
